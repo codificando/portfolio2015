63268375649a4fcf4b21433eed5a9513983269fa
Ingrid Medeiros <i.smedeiros@hotmail.com>
2017-12-13 23:12:46 -0200

    Model Projeto

---
+++ app/Model/Projeto.php
@@ -0,0 +1,11 @@
+<?php
+class Projeto extends AppModel {
+
+    public $name = 'Projeto';
+
+    public $validate = array(
+       /* 'imagem'=> array(
+            'rule' => 'notEmpty'
+        )*/
+    );
+}
